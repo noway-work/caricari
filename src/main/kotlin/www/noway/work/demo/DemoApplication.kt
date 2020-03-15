b06ee09ca50a21218882de417ca982611e9c60be
yoshinobu.inoue <yoshinobu.inoue@emtg.co.jp>
2020-03-15 12:21:29 +0900

    first commit

---
+++ src/main/kotlin/www/noway/work/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package www.noway.work.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
---
+++ src/test/kotlin/www/noway/work/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package www.noway.work.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
